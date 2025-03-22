fb53ccebc0b8e88ed39be69d9cc4d97b457ba7f2
nojaf <florian.verdonck@outlook.com>
2025-03-22 14:45:19 +0100

    Use base in vite config

--- tool/vite.config.mjs
+++ tool/vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/vanilla-jsx-to-rescript-jsx/",
   build: {
     target: "es2022", // napi-rs wasi-browser.js output relies on top-level await
   },
